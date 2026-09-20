6dfb02711ad9d6b41fc92333196db628b2079cd9
njahnavi24 <jahnavinagavalli24@gmail.com>
2026-09-20 18:38:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your code here
+let divs = document.querySelectorAll('.image')
+divs.forEach(function(div){
+	div.draggable = true
+	div.addEventListsner("dragstart",(e)=>{
+		e.dataTransfer.setData("text",e.target.id)
+	})
+	div.addEventListener("dragover",(e)=>{
+		e.preventDefault()
+	})
+	div.addEventListener("drop",(e)=>{
+		e.preventDefault()
+		let draggedId = e.dataTransfer.getData("text")
+		let draggedDiv = document.getElementById(draggedId)
+		let droppedDiv = e.target
+		let temp = draggedDiv.style.backgroundImage
+		draggedDiv.style.backgdoundImage = droppedDiv.style.backgroundImage
+		droppedDiv.style.backgroundImage = temp
+	})
+})
